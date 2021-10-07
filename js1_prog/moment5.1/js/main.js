0b1df9f8e023ec5102ab1c005d0056516f5fb9ed
Kristina Abrahamsson <kristina_ab@hotmail.com>
2021-10-07 18:04:36 +0200

    add comments

--- js1_prog/moment5.1/js/main.js
+++ js1_prog/moment5.1/js/main.js
@@ -1,22 +1,31 @@
 
 "use strict";
 
+//Global variables
 let infoEl;
 let sitesEl;
 
+//Function onlad window
 function init() {
+    //Element refrences
     infoEl = document.getElementById("info");
     sitesEl = document.getElementById("sites");
-    loadInfo();
+
+    //Run loadData
+    loadData();
 }
 
+//When window load run init
 window.addEventListener("load", init);
 
-function loadInfo() {
+//Get data from student.json through AJAX 
+function loadData() {
     let xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
+        if (this.readyState == 4 && this.status == 200) { //If succesful load
+            //Create new object from json-file
             let obj = JSON.parse(this.responseText);
+            //run functions
             showInfo(obj.student.information);
             showSites(obj.student.websites);
         }
@@ -25,53 +34,61 @@ function loadInfo() {
     xhttp.send();
 }
 
+// Get information from object, modify DOM and present data in info slot
 function showInfo(info) {
+    // Create and print name element
     let strongNameEl = document.createElement("strong");
     let strongName = document.createTextNode("Namn: ");
     strongNameEl.appendChild(strongName);
     infoEl.appendChild(strongNameEl);
+    infoEl.innerHTML += info.name; //Print name from info
 
-    infoEl.innerHTML += info.name;
-
+    //Create and print br element
     let brEl = document.createElement("br");
     infoEl.appendChild(brEl);
 
+    //Create and print email element
     let strongEmailEl = document.createElement("strong");
     let strongEmail = document.createTextNode("E-post: ");
     strongEmailEl.appendChild(strongEmail);
     infoEl.appendChild(strongEmailEl);
 
+    //Create and print link element to email
     let emailLinkEl = document.createElement("a");
     emailLinkEl.href = "mailto:" + info.email;
-    emailLinkEl.innerHTML = info.email;
+    emailLinkEl.innerHTML = info.email; //Print email from info
     infoEl.appendChild(emailLinkEl);
 
+    //Create and print 2:nd br element
     let brEl2 = document.createElement("br");
     infoEl.appendChild(brEl2);
 
+    //Create and print website element
     let strongWebEl = document.createElement("strong");
     let strongWebText = document.createTextNode("Webbsida: ");
     strongWebEl.appendChild(strongWebText);
     infoEl.appendChild(strongWebEl);
 
+    //Create and print website link
     let webLinkEl = document.createElement("a");
     webLinkEl.href = info.website;
     webLinkEl.target = "_blank";
-    webLinkEl.innerHTML = info.website + "<br>";
+    webLinkEl.innerHTML = info.website + "<br>"; //Print website from info
     infoEl.appendChild(webLinkEl);
 }
 
+//Print list of websites from object and modify DOM
 function showSites(sites) {
-    let websites = sites.websites;
-
+    //Run through list and add list element with link to website
     for (let i = 0; i < sites.length; i++) {
         let liEl = document.createElement("li");
         let linkEl = document.createElement("a");
-        linkEl.href = sites[i].siteurl;
+        linkEl.href = sites[i].siteurl; //Print siteurl from sites
         linkEl.target = "_blank";
-        linkEl.title = sites[i].description;
-        linkEl.innerHTML = sites[i].sitename;
+        linkEl.title = sites[i].description; //Add site description to link title
+        linkEl.innerHTML = sites[i].sitename; //Print sitename 
 
+        //append new elemet to sites element
         liEl.appendChild(linkEl);
         sitesEl.appendChild(liEl);
     }
--- js1_prog/moment5.2/js/main.js
+++ js1_prog/moment5.2/js/main.js
@@ -1,35 +1,50 @@
 
 "use strict";
 
+//Global variables
 let infoEl;
 let sitesEl;
 
+//Function onlad window
 function init() {
+    //Element refrences
     infoEl = document.getElementById("info");
     sitesEl = document.getElementById("sites");
-    loadInfo();
+
+    //Run loadData
+    loadData();
 }
 
+//When window load run init
 window.addEventListener("load", init);
 
-function loadInfo(){
+//Load data to site with fetch
+function loadData() {
     fetch("https://modest-bose-aa97d3.netlify.app/js1_prog/Moment%204.2/student.json")
-    .then(response => response.json())
-    .then(info => {
-        showInfo(info.student.information);
-        showSites(info.student.websites);
-    })
+        .then(response => response.json()) //Create object from json
+        .then(info => {
+            //run functions with object data
+            showInfo(info.student.information);
+            showSites(info.student.websites);
+        })
 }
 
-function showInfo(info){
+//Add elements and information to DOM from object
+function showInfo(info) {
+    //Print name element and name from info
     infoEl.innerHTML = "<strong>Namn: </strong>" + info.name + "<br>";
+
+    //Print email element and email from info
     infoEl.innerHTML += "<strong>E-post: </strong>" + "<a href='mailto:" + info.email + "'>" + info.email + "</a> <br>";
+
+    //Print website element and website from info
     infoEl.innerHTML += "<strong>Webbplats: </strong>" + "<a href='" + info.website + "' target='_blank'>" + info.website + "<br></a>";
 }
 
-function showSites(websites){
-
-    for (let i = 0; i < websites.length; i++){
+//Add list of sites to DOM from object
+function showSites(websites) {
+    //Run through list of websites and create a new list element
+    for (let i = 0; i < websites.length; i++) {
         sitesEl.innerHTML += "<li><a href='" + websites[i].siteurl + "' target='_blank' title='" + websites[i].description + "'>" + websites[i].sitename + "</a></li>";
     }
 }
