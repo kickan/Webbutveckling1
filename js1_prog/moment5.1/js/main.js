d41ec59b2c26b87f4f07105c85164ee040e7b96d
Kristina Abrahamsson <kristina_ab@hotmail.com>
2021-10-07 17:40:26 +0200

    moment 5.2

--- js1_prog/moment5.1/js/main.js
+++ js1_prog/moment5.1/js/main.js
@@ -16,23 +16,22 @@ function loadInfo() {
     let xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
-            let infoObj = JSON.parse(this.responseText);
-            showInfo(infoObj);
-            showSites(infoObj);
-
+            let obj = JSON.parse(this.responseText);
+            showInfo(obj.student.information);
+            showSites(obj.student.websites);
         }
     };
     xhttp.open('GET', 'https://modest-bose-aa97d3.netlify.app/js1_prog/Moment%204.2/student.json', true);
     xhttp.send();
 }
 
-function showInfo(infoObj) {
+function showInfo(info) {
     let strongNameEl = document.createElement("strong");
     let strongName = document.createTextNode("Namn: ");
     strongNameEl.appendChild(strongName);
     infoEl.appendChild(strongNameEl);
 
-    infoEl.innerHTML += infoObj.student.information.name;
+    infoEl.innerHTML += info.name;
 
     let brEl = document.createElement("br");
     infoEl.appendChild(brEl);
@@ -43,35 +42,35 @@ function showInfo(infoObj) {
     infoEl.appendChild(strongEmailEl);
 
     let emailLinkEl = document.createElement("a");
-    emailLinkEl.href = "mailto:" + infoObj.student.information.email;
-    emailLinkEl.innerHTML = infoObj.student.information.email;
+    emailLinkEl.href = "mailto:" + info.email;
+    emailLinkEl.innerHTML = info.email;
     infoEl.appendChild(emailLinkEl);
 
     let brEl2 = document.createElement("br");
     infoEl.appendChild(brEl2);
 
     let strongWebEl = document.createElement("strong");
-    let strongWebText = document.createTextNode("Webbsida:");
+    let strongWebText = document.createTextNode("Webbsida: ");
     strongWebEl.appendChild(strongWebText);
     infoEl.appendChild(strongWebEl);
 
     let webLinkEl = document.createElement("a");
-    webLinkEl.href = infoObj.student.information.website;
+    webLinkEl.href = info.website;
     webLinkEl.target = "_blank";
-    webLinkEl.innerHTML = infoObj.student.information.website + "<br>";
+    webLinkEl.innerHTML = info.website + "<br>";
     infoEl.appendChild(webLinkEl);
 }
 
-function showSites(sitesObj) {
-    let websites = sitesObj.student.websites;
+function showSites(sites) {
+    let websites = sites.websites;
 
-    for (let i = 0; i < websites.length; i++) {
+    for (let i = 0; i < sites.length; i++) {
         let liEl = document.createElement("li");
         let linkEl = document.createElement("a");
-        linkEl.href = websites[i].siteurl;
+        linkEl.href = sites[i].siteurl;
         linkEl.target = "_blank";
-        linkEl.title = websites[i].description;
-        linkEl.innerHTML = websites[i].sitename;
+        linkEl.title = sites[i].description;
+        linkEl.innerHTML = sites[i].sitename;
 
         liEl.appendChild(linkEl);
         sitesEl.appendChild(liEl);
--- js1_prog/moment5.2/js/main.js
+++ js1_prog/moment5.2/js/main.js
@@ -1,3 +1,35 @@
-/*
-Här lägger du din JavaScript-kod
-*/
+
+"use strict";
+
+let infoEl;
+let sitesEl;
+
+function init() {
+    infoEl = document.getElementById("info");
+    sitesEl = document.getElementById("sites");
+    loadInfo();
+}
+
+window.addEventListener("load", init);
+
+function loadInfo(){
+    fetch("student.json")
+    .then(response => response.json())
+    .then(info => {
+        showInfo(info.student.information);
+        showSites(info.student.websites);
+    })
+}
+
+function showInfo(info){
+    infoEl.innerHTML = "<strong>Namn: </strong>" + info.name + "<br>";
+    infoEl.innerHTML += "<strong>E-post: </strong>" + "<a href='mailto:" + info.email + "'>" + info.email + "</a> <br>";
+    infoEl.innerHTML += "<strong>Webbplats: </strong>" + "<a href='" + info.website + "' target='_blank'>" + info.website + "<br></a>";
+}
+
+function showSites(websites){
+
+    for (let i = 0; i < websites.length; i++){
+        sitesEl.innerHTML += "<li><a href='" + websites[i].siteurl + "' target='_blank' title='" + websites[i].description + "'>" + websites[i].sitename + "</a></li>";
+    }
+}
